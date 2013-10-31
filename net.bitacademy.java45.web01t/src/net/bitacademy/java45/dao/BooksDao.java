dfdd989fd357cc44e2a0d42dd5c99752dab91c6e
eomjinyoung <jinyoung.eom@gmail.com>
2013-10-31 11:12:07 +0900

    .

---
+++ net.bitacademy.java45.web01t/src/net/bitacademy/java45/dao/BooksDao.java
@@ -0,0 +1,76 @@
+package net.bitacademy.java45.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.bitacademy.java45.vo.Book;
+
+public class BooksDao {
+	/**
+	 * @param keywords
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Book> select(List<String> keywords) throws Exception {
+		Connection conn = null;
+	    Statement stmt = null;
+	    ResultSet rset = null;
+	    
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+	        conn = DriverManager.getConnection(
+	            "jdbc:mysql://localhost/ebookshop", "root", "1234");  // <<== Check
+	        stmt = conn.createStatement();
+	         
+	        String sqlStr = "SELECT * FROM books WHERE";
+	        if (keywords.size() > 0) {
+	        	sqlStr += "author IN (";
+	        	for(int i = 0; i < keywords.size(); i++) {
+	        		if (i > 0) {
+	        			sqlStr += ",";
+	        		}
+	        		sqlStr += "'" + keywords.get(i) + "'";
+	        	}
+	        	sqlStr += ") AND";
+	        }
+	        sqlStr += "qty > 0 ORDER BY author ASC, title ASC";
+	        
+	        rset = stmt.executeQuery(sqlStr);
+	        
+	        ArrayList<Book> list = new ArrayList<Book>();
+	        Book book = null;
+	        
+	        while(rset.next()) {
+	            book = new Book();
+	            book.setId(rset.getInt("id"));
+	            book.setTitle(rset.getString("title"));
+	            book.setAuthor(rset.getString("author"));
+	            book.setPrice(rset.getFloat("price"));
+	            
+	            list.add(book);
+	        }
+	        
+	        return list;
+	        
+		} catch (Exception e) {
+			throw e;
+			
+		} finally {
+			try {if (rset != null) rset.close();} catch(Exception e) {}
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+	        try {if (conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+}
+
+
+
+
+
+
+
---
+++ net.bitacademy.java45.web01t/src/net/bitacademy/java45/vo/Book.java
@@ -0,0 +1,42 @@
+package net.bitacademy.java45.vo;
+
+public class Book {
+	protected int 		id;
+	protected String 	title;
+	protected String 	author;
+	protected float 	price;
+	protected int 		qty;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	
+}
