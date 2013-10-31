3c8de7862a2dd4aa9a1c630ff0fb4c1761891f01
eomjinyoung <jinyoung.eom@gmail.com>
2013-10-31 14:22:11 +0900

    .

---
+++ net.bitacademy.java45.web01t/src/net/bitacademy/java45/servlets/book/BookListServlet.java
@@ -0,0 +1,45 @@
+package net.bitacademy.java45.servlets.book;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.bitacademy.java45.dao.BooksDao;
+import net.bitacademy.java45.vo.Book;
+
+@WebServlet("/book/list")
+public class BookListServlet extends HttpServlet {
+	@Override
+	protected void doGet(
+			HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		BooksDao booksDao = new BooksDao();
+		try {
+			List<Book> list = booksDao.select(
+					request.getParameterValues("author"));
+			
+			//출력은 JSP에게 맡긴다.
+			request.setAttribute("list", list);
+			
+			RequestDispatcher rd = 
+					request.getRequestDispatcher("/book/BookList.jsp");
+			rd.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
